a4681e221aab58d1ba2fe5098c1ca95994dab8d2
Caiyi Hsu <cai1hsu@outlook.com>
2023-11-27 18:25:29 +0800

    Add headless mode support to Program.cs

--- DanmakuEngine.TH08/Program.cs
+++ DanmakuEngine.TH08/Program.cs
@@ -1,8 +1,28 @@
 ﻿using DanmakuEngine.Engine;
 using DanmakuEngine.Engine.Platform;
 using DanmakuEngine.TH08.Games;
+using DanmakuEngine.Logging;
 
-using (var host = DesktopGameHost.GetSuitableHost())
+internal class Program
 {
-    host.Run(new TH08Game());
+
+    [STAThread]
+    private static void Main(string[] args)
+    {
+        var game = new TH08Game();
+
+        #if HEADLESS
+            Logger.Debug("Running in headless mode.");
+
+            using (var host = new HeadlessGameHost(() => true))
+            {
+                host.Run(game);
+            }
+        #else
+            using (var host = DesktopGameHost.GetSuitableHost())
+            {
+                host.Run(game);
+            }
+        #endif
+    }
 }
