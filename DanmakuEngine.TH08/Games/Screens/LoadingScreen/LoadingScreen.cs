8c0b7a9e3c47f8d7dd0dfaf880282f6ded26573a
Caiyi Hsu <cai1hsu@outlook.com>
2023-12-03 19:01:35 +0800

    reimplement `LoadingScreen` with `Scheduler`

--- DanmakuEngine.TH08/Games/Screens/LoadingScreen/LoadingScreen.cs
+++ DanmakuEngine.TH08/Games/Screens/LoadingScreen/LoadingScreen.cs
@@ -1,10 +1,13 @@
 ﻿using DanmakuEngine.Logging;
 using DanmakuEngine.Games.Screens.MainMenu;
+using DanmakuEngine.Scheduling;
 
 namespace DanmakuEngine.Games.Screens.Welcome;
 
 public class LoadingScreen : Screen
 {
+    private Scheduler scheduler = null!;
+
     // All of the three methods below run in the `Update` loop
 
     // called by `load` in constructor for screen and manually called for other objects
@@ -13,12 +16,17 @@ public class LoadingScreen : Screen
     public override void Load()
     {
         keyboardHandler = null!;
+
+        InternalChildren =
+        [
+            scheduler = new Scheduler(),
+        ];
     }
 
     // This method is called the first frame when the screen(or average object) is completely loaded
     // If you want to do something that takes a long time without blocking the main thread, you can use `Start` method
     // make it async and use `await Task.Delay` to delay the execution
-    public override async void Start()
+    public override void Start()
     {
         Logger.Log("少女祈祷中");
         Logger.Log("    Now loading...");
@@ -28,9 +36,19 @@ public class LoadingScreen : Screen
         Logger.Info("Keyboard is not handled in this screen.");
 
         // Assume that we are loading something
-        await Task.Delay(2000);
+        // you should load your resources here
+        // and do it in a async way
 
-        ScreenStack.Switch(new MainScreen());
+        scheduler.ScheduleTask(new ScheduledTask(() =>
+        {
+            // if finished, switch to main screen
+            ScreenStack.Switch(new MainScreen());
+        }, () =>
+        {
+            // check that if our loading is finished
+            // here we just check if the time is greater than 2s
+            return ScreenClock.CurrentTime > 2;
+        }));
     }
 
     // This method is called every frame, the first call is after `Start` method
