9ff76e7ba93f5c6bd2ccecfcf3cd00fb008ba684
Caiyi Hsu <cai1hsu@outlook.com>
2023-12-03 19:00:34 +0800

    Add IUpdatable interface to Scheduler class

--- DanmakuEngine/Scheduling/Scheduler.cs
+++ DanmakuEngine/Scheduling/Scheduler.cs
@@ -1,6 +1,8 @@
+using DanmakuEngine.Games;
+
 namespace DanmakuEngine.Scheduling;
 
-public class Scheduler
+public class Scheduler : IUpdatable
 {
     private readonly Queue<ScheduledTask> tasks = new();
     private readonly object taskLock = new();
@@ -13,10 +15,26 @@ public class Scheduler
         }
     }
 
+    public void load()
+        => Load();
+
+    public virtual void Load()
+    {
+    }
+
+    public void start()
+        => Start();
+
+    public virtual void Start()
+    {
+    }
+
     public void update()
     {
         lock (taskLock)
         {
+            var next = new Queue<ScheduledTask>();
+
             while (tasks.Count > 0)
             {
                 var task = tasks.Dequeue();
@@ -27,7 +45,14 @@ public class Scheduler
 
                     task.Run();
                 }
+                else
+                {
+                    next.Enqueue(task);
+                }
             }
+
+            while (next.Count > 0)
+                tasks.Enqueue(next.Dequeue());
         }
 
         Update();
