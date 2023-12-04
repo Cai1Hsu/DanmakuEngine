5f276219352eebb5dbb299d36924867661a20d24
Caiyi Hsu <cai1hsu@outlook.com>
2023-12-04 18:34:26 +0800

    partial TestGame

--- DanmakuEngine.Tests/Timing/TestGame.cs
+++ DanmakuEngine.Tests/Timing/TestGame.cs
@@ -2,7 +2,7 @@ using DanmakuEngine.Games;
 
 namespace DanmakuEngine.Tests.Timing;
 
-public class TestGame : Game
+public partial class TestGame : Game
 {
     public override string Name => $"{base.Name} TestGame";
 }
