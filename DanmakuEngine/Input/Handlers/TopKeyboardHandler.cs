09f11034f6635b30663eabef6e6cdc93d285bed6
Caiyi Hsu <cai1hsu@outlook.com>
2023-12-04 17:56:47 +0800

    fix duplicate inject call in  `TopKeyboardHandler`

--- DanmakuEngine/Input/Handlers/TopKeyboardHandler.cs
+++ DanmakuEngine/Input/Handlers/TopKeyboardHandler.cs
@@ -14,21 +14,7 @@ public partial class TopKeyboardHandler : IInputHandler
 
     public TopKeyboardHandler()
     {
-        // FIXME: Causes a null reference exception when NativeAOT is enabled
-        /*
-            Dependency/IInjectable.cs(16): 
-                Trim analysis warning IL2075: 
-                    DanmakuEngine.Dependency.IInjectable.AutoInject(Boolean): 
-                        'this' argument does not satisfy 'DynamicallyAccessedMemberTypes.PublicFields', 'DynamicallyAccessedMemberTypes.NonPublicFields' in call to 'System.Type.GetFields(BindingFlags)'. The return value of method 'System.Object.GetType()' does not have matching annotations. The source value must declare at least the same requirements as those declared on the target location it is assigned to. 
-            Dependency/IInjectable.cs(37): 
-                Trim analysis warning IL2075: 
-                    DanmakuEngine.Dependency.IInjectable.AutoInject(Boolean): 
-                        'this' argument does not satisfy 'DynamicallyAccessedMemberTypes.PublicProperties', 'DynamicallyAccessedMemberTypes.NonPublicProperties' in call to 'System.Type.GetProperties(BindingFlags)'. The return value of method 'System.Object.GetType()' does not have matching annotations. The source value must declare at least the same requirements as those declared on the target location it is assigned to. 
-        */
-
-        ((IInjectable)this).AutoInject();
-
-        // Failed under NativeAOT
+        // check that if we inject the screen stack successfully
         Debug.Assert(screens != null);
     }
 
