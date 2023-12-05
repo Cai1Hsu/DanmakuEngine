2bd7f1dd65e89570996c9a2bd7e05044718be417
Caiyi Hsu <cai1hsu@outlook.com>
2023-12-05 17:05:53 +0800

    implement `ScheduleTaskDelay`

--- DanmakuEngine/Scheduling/Scheduler.cs
+++ DanmakuEngine/Scheduling/Scheduler.cs
@@ -1,4 +1,9 @@
+using System.Data;
+using System.Linq.Expressions;
 using DanmakuEngine.Games;
+using DanmakuEngine.Logging;
+using DanmakuEngine.Timing;
+using Silk.NET.Vulkan;
 
 namespace DanmakuEngine.Scheduling;
 
@@ -6,6 +11,21 @@ public class Scheduler : IUpdatable
 {
     private readonly Queue<ScheduledTask> tasks = new();
     private readonly object taskLock = new();
+
+    private double CurrentTime => Clock.CurrentTime;
+
+    private Clock? clock = null;
+
+    private Clock Clock
+    {
+        get
+        {
+            if (clock is not null)
+                return clock;
+
+            return clock = new Clock(true);
+        }
+    }
 
     public void ScheduleTask(ScheduledTask task)
     {
@@ -20,6 +40,38 @@ public class Scheduler : IUpdatable
 
     public void ScheduleTask(Action action)
         => ScheduleTask(new ScheduledTask(action));
+
+    /// <summary>
+    /// Schedules a task to run after a delay.
+    /// </summary>
+    /// <param name="task">The task to schedule</param>
+    /// <param name="delay">delay in seconds</param>
+    public void ScheduleTaskDelay(Action action, double delay)
+    {
+        if (delay <= 0)
+        {
+            ScheduleTask(action);
+            return;
+        }
+
+        var untilTime = CurrentTime + delay;
+
+        ScheduleTask(
+            scheduleDelayedTask,
+            isDelayFinished
+        );
+
+        void scheduleDelayedTask()
+            => ScheduleTask(action);
+
+        bool isDelayFinished()
+        {
+            if (delay <= 0)
+                return true;
+
+            return CurrentTime >= untilTime;
+        }
+    }
 
     public void load()
         => Load();
@@ -64,6 +116,10 @@ public class Scheduler : IUpdatable
         Update();
     }
 
+    /// <summary>
+    /// Please do not call this methods directly
+    /// you should override <see cref="Update"/> and call <see cref="update"/> instead
+    /// </summary>
     public virtual void Update()
     {
 
