efe676ac8d61f3eefab081f6c9b5db93e536326c
Caiyi Hsu <cai1hsu@outlook.com>
2023-12-05 17:06:07 +0800

    add test coverage for `Scheduler`

---
+++ DanmakuEngine.Tests/Scheduling/TestScheduler.cs
@@ -0,0 +1,78 @@
+using DanmakuEngine.Engine;
+using DanmakuEngine.Scheduling;
+using DanmakuEngine.Timing;
+using DanmakuEngine.Arguments;
+using DanmakuEngine.Tests.Games;
+using NUnit.Framework;
+
+namespace DanmakuEngine.Tests.Scheduling;
+
+public class TestScheduler
+{
+    private ArgumentProvider defaultProvider = null!;
+
+    [SetUp]
+    public void SetUp()
+    {
+        defaultProvider = ArgumentProvider.CreateDefaultProvider(new ParamTemplate(), Array.Empty<string>());
+    }
+
+    [Test]
+    public void TestScheduleTask()
+    {
+        var testGame = new TestGame();
+
+        using var host = new HeadlessGameHost(5000);
+
+        var scheduler = new Scheduler();
+
+        host.OnLoad += h =>
+        {
+            scheduler.ScheduleTask(() => h.RequestClose());
+        };
+
+        host.OnUpdate += h => scheduler.update();
+
+        host.OnTimedout += Assert.Fail;
+
+        host.Run(testGame, defaultProvider);
+    }
+
+    [Test]
+    public void TestScheduleTaskDelay()
+    {
+        const double test_seconds = 0.1;
+
+        var testGame = new TestGame();
+
+        using var host = new HeadlessGameHost(5000);
+
+        var clock = new Clock(true);
+
+        var scheduler = new Scheduler();
+
+        double current_time = 0;
+
+        host.OnLoad += h =>
+        {
+            // We didn't start the clock on construction, because the `Time` was not cleared yet
+            clock.Start();
+
+            current_time = clock.CurrentTime;
+
+            scheduler.ScheduleTaskDelay(() => h.RequestClose(), test_seconds);
+        };
+
+        host.OnUpdate += _ =>
+        {
+            scheduler.update();
+            current_time = Math.Max(current_time, clock.CurrentTime);
+        };
+
+        host.OnTimedout += Assert.Fail;
+
+        host.Run(testGame, defaultProvider);
+
+        Assert.That(current_time, Is.GreaterThanOrEqualTo(test_seconds));
+    }
+}
