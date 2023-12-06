2e3403f2bb45090ebe54ba6f2509f67a5abdfb16
Caiyi Hsu <cai1hsu@outlook.com>
2023-12-06 14:33:37 +0800

    implement `LazyLoadValue<T>`

---
+++ DanmakuEngine/Allocations/LazyLoadValue.cs
@@ -0,0 +1,57 @@
+using System.Diagnostics.CodeAnalysis;
+
+namespace DanmakuEngine.Allocations;
+
+/// <summary>
+/// A lazy load value that will create an instance of <typeparamref name="T"/> when <see cref="Value"/> is first accessed
+/// 
+/// You must select and pass correct Parameters for the constructor of <typeparamref name="T"/> when creating an instance of this class
+/// </summary>
+/// <typeparam name="T">The type of the instance</typeparam>
+public class LazyLoadValue<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors | DynamicallyAccessedMemberTypes.PublicParameterlessConstructor)]T>
+{
+    private T? _value;
+
+    private readonly object[] parameters;
+
+    /// <summary>
+    /// Create a lazy load instance of <typeparamref name="T"/> with no parameters
+    /// </summary>
+    private LazyLoadValue()
+    {
+        parameters = null!;
+    }
+
+    /// <summary>
+    /// Create a lazy load instance of <typeparamref name="T"/> with parameters for the constructor
+    /// </summary>
+    /// <param name="parameters"></param>
+    public LazyLoadValue(params object[] parameters)
+    {
+        this.parameters = parameters;
+    }
+
+    public T Value
+    {
+        get
+        {
+            if (_value is null)
+            {
+                if (parameters is null)
+                    _value = Activator.CreateInstance<T>()!;
+                else
+                    _value = (T)Activator.CreateInstance(typeof(T), parameters)!;
+
+                if (_value is null)
+                    throw new LazyLoadValueException($"Failed to create instance of {typeof(T)}");
+            }
+
+            return _value;
+        }
+    }
+
+    private class LazyLoadValueException(string message)
+        : Exception(message)
+    {
+    }
+}
