630b2b2fde87e69b6284c087ffe59effe642bc1a
Caiyi Hsu <cai1hsu@outlook.com>
2023-12-17 21:38:25 +0800

    Refactor FixedLinearMovement classes to use lowercase variable names

--- DanmakuEngine/Movements/FixedLinearMovement.cs
+++ DanmakuEngine/Movements/FixedLinearMovement.cs
@@ -9,35 +9,35 @@ public abstract class FixedLinearMovementBase<T> : MovementBase<T>, ICanGetSpeed
 
     protected abstract override T CurrentValue();
 
-    public FixedLinearMovementBase(T Speed)
+    public FixedLinearMovementBase(T speed)
     {
-        this.Speed = Speed;
+        this.Speed = speed;
     }
 }
 
-public class FixedLinearMovementF(float Speed)
-    : FixedLinearMovementBase<float>(Speed)
+public class FixedLinearMovementF(float speed)
+    : FixedLinearMovementBase<float>(speed)
 {
     protected override float CurrentValue()
         => startValue + Speed * (float)ElapsedTime;
 }
 
-public class FixedLinearMovementD(double Speed)
-    : FixedLinearMovementBase<double>(Speed)
+public class FixedLinearMovementD(double speed)
+    : FixedLinearMovementBase<double>(speed)
 {
     protected override double CurrentValue()
         => startValue + Speed * ElapsedTime;
 }
 
-public class FixedLinearMovementV2F(Vector2D<float> Speed)
-    : FixedLinearMovementBase<Vector2D<float>>(Speed)
+public class FixedLinearMovementV2F(Vector2D<float> speed)
+    : FixedLinearMovementBase<Vector2D<float>>(speed)
 {
     protected override Vector2D<float> CurrentValue()
         => startValue + Speed * (float)ElapsedTime;
 }
 
-public class FixedLinearMovementV2D(Vector2D<double> Speed)
-    : FixedLinearMovementBase<Vector2D<double>>(Speed)
+public class FixedLinearMovementV2D(Vector2D<double> speed)
+    : FixedLinearMovementBase<Vector2D<double>>(speed)
 {
     protected override Vector2D<double> CurrentValue()
         => startValue + Speed * ElapsedTime;
