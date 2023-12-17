9ca39e362ca2e7dddb7e83a41369f6cbf643d44c
Caiyi Hsu <cai1hsu@outlook.com>
2023-12-17 21:45:54 +0800

    Update gravity constants in GravityDropMovement classes

--- DanmakuEngine/Movements/GravityDropMovement.cs
+++ DanmakuEngine/Movements/GravityDropMovement.cs
@@ -3,37 +3,37 @@ using Silk.NET.Maths;
 namespace DanmakuEngine.Movements;
 
 public class GravityDropMovementF(float initialSpeed)
-    : FixedAcceleratedMovementF(initialSpeed, GRAVITY)
+    : FixedAcceleratedMovementF(initialSpeed, GRAVITY_F)
 {
     /// <summary>
     /// You must be very familiar with this number
     /// </summary>
-    public const float GRAVITY = 9.81f;
+    public const float GRAVITY_F = 9.81f;
 }
 
 public class GravityDropMovementD(double initialSpeed)
-    : FixedAcceleratedMovementD(initialSpeed, GRAVITY)
+    : FixedAcceleratedMovementD(initialSpeed, GRAVITY_D)
 {
     /// <summary>
     /// You must be very familiar with this number
     /// </summary>
-    public const double GRAVITY = 9.81;
+    public const double GRAVITY_D = 9.81;
 }
 
 public class GravityDropMovementV2F(Vector2D<float> initialSpeed)
-    : FixedAcceleratedMovementV2F(initialSpeed, GRAVITY)
+    : FixedAcceleratedMovementV2F(initialSpeed, GRAVITY_V2F)
 {
     /// <summary>
     /// You must be very familiar with this number
     /// </summary>
-    public static readonly Vector2D<float> GRAVITY = new(0, -9.81f);
+    public static readonly Vector2D<float> GRAVITY_V2F = new(0, -9.81f);
 }
 
 public class GravityDropMovementV2D(Vector2D<double> initialSpeed)
-    : FixedAcceleratedMovementV2D(initialSpeed, GRAVITY)
+    : FixedAcceleratedMovementV2D(initialSpeed, GRAVITY_V2D)
 {
     /// <summary>
     /// You must be very familiar with this number
     /// </summary>
-    public static readonly Vector2D<double> GRAVITY = new(0, -9.81);
+    public static readonly Vector2D<double> GRAVITY_V2D = new(0, -9.81);
 }
