cbcde43ede24adde49dc4f4f3c8190ecfc186e71
Caiyi Hsu <cai1hsu@outlook.com>
2023-12-19 22:15:10 +0800

    initialize delegate with null!

--- DanmakuEngine.TH08/Games/Screens/MainScreen/SecretCodeHandler.cs
+++ DanmakuEngine.TH08/Games/Screens/MainScreen/SecretCodeHandler.cs
@@ -34,7 +34,7 @@ public class SecretCodeHandler
 
     private double lastKeyDown = -1f;
 
-    public Action OnSecretCodeEntered { get; set; }
+    public Action OnSecretCodeEntered { get; set; } = null!;
 
     public bool HandleKey(Keysym key)
     {
