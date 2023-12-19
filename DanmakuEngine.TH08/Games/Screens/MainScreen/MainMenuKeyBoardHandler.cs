25e92af69337c09a38652dcc798b63d6b4b71e25
Caiyi Hsu <cai1hsu@outlook.com>
2023-12-19 16:31:27 +0800

    migrate old code

--- DanmakuEngine.TH08/Games/Screens/MainScreen/MainMenuKeyBoardHandler.cs
+++ DanmakuEngine.TH08/Games/Screens/MainScreen/MainMenuKeyBoardHandler.cs
@@ -8,33 +8,40 @@ namespace DanmakuEngine.Games.Screens.MainMenu;
 public partial class MainMenuKeyBoardHandler : UserKeyboardHandler
 {
     [Inject]
-    private ScreenStack screens = null!;
+    private ScreenStack _screens = null!;
 
     public SecretCodeHandler secretCodeHandler = null!;
 
     private bool cheating = false;
 
-    public override void KeyDown(Keysym key, bool _)
+    protected override void RegisterKeys()
     {
         // DEMO: Pressing escape closes the game
-
-        if (key.Sym == (int)KeyCode.KEscape)
+        Register(KeyCode.KEscape).OnDown += (_, _) =>
         {
             // TODO: add this action to Scheduler
             // We should handle actions in the Update loop
             // This is needed to avoid concurrency issues
             // as we are planning to use multiple threads
-            while (!screens.Empty())
-                screens.Pop();
-        }
+            while (!_screens.Empty())
+                _screens.Pop();
+        };
 
-        if (secretCodeHandler != null)
+        secretCodeHandler.OnSecretCodeEntered += delegate
         {
-            if (!cheating && secretCodeHandler.HandleKey(key))
-            {
-                Logger.Error("😠 You are cheating!");
-                cheating = true;
-            }
-        }
+            Logger.Error("😠 You are cheating!");
+            cheating = true;
+        };
+    }
+
+    public override bool HandleEvent(KeyboardEvent e)
+    {
+        var handled = base.HandleEvent(e);
+
+        handled |= !cheating &&
+                   secretCodeHandler is not null &&
+                   secretCodeHandler.HandleKey(e.Keysym);
+
+        return handled;
     }
 }
