7c5c7aaf2505a6b404934d60e5036f3d3ba57a38
Caiyi Hsu <cai1hsu@outlook.com>
2024-03-31 21:44:57 +0800

    removing unused ImguiExtensions methods

--- DanmakuEngine/DearImgui/ImguiExtensions.cs
+++ DanmakuEngine/DearImgui/ImguiExtensions.cs
@@ -26,43 +26,5 @@ public static unsafe class ImguiExtensions
         _rhs->Data = _lhs->Data;
         _lhs->Data = rhs_data;
     }
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static void Reserve<T>(this ref ImVector lhs, int new_capacity)
-        where T : struct
-    {
-        int sizeofT = Marshal.SizeOf<T>();
-
-        Reserve(ref lhs, new_capacity, sizeofT);
-    }
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static void Reserve(this ref ImVector lhs, int new_capacity, int sizeofT)
-    {
-        Debug.Assert(new_capacity >= 0, $"new_capacity: {new_capacity}");
-
-        fixed (void* p_lhs = &lhs)
-        {
-            if (new_capacity <= lhs.Capacity)
-                return;
-
-            nint new_data = (nint)ImguiUtils.ImAlloc(new_capacity * sizeofT);
-
-            if (lhs.Data != 0)
-            {
-                long size = lhs.Size * sizeofT;
-                // memcpy(new_data, Data, (size_t)Size * sizeof(T));
-                Buffer.MemoryCopy((void*)lhs.Data, (void*)new_data, size, size);
-                // IM_FREE(Data);
-                ImguiUtils.ImFree(lhs.Data);
-            }
-
-            ImguiVector* _lhs = (ImguiVector*)p_lhs;
-            // lhs->Data = new_data;
-            _lhs->Data = new_data;
-            // lhs->Capacity = new_capacity;
-            _lhs->Capacity = new_capacity;
-        }
-    }
 }
 
