c214b71128120dc44db5bebe1f6c2b54ff0850a9
Caiyi Hsu <cai1hsu@outlook.com>
2024-03-31 20:42:12 +0800

    Add paramless constructor for `ImguiWindow`

--- DanmakuEngine/DearImgui/Windowing/ImguiWindow.cs
+++ DanmakuEngine/DearImgui/Windowing/ImguiWindow.cs
@@ -5,6 +5,12 @@ namespace DanmakuEngine.DearImgui.Windowing;
 public class ImguiWindow(string title)
     : ImguiWindowBase(title)
 {
+    public ImguiWindow()
+        : this(string.Empty)
+    {
+        BypassBeginEnd = true;
+    }
+
     public event Action? OnUpdate = null!;
 
     protected override void Update()
