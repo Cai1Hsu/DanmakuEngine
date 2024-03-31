f3f7350ac35a5db34c2f198ad035c5ba1f43d2dc
Caiyi Hsu <cai1hsu@outlook.com>
2024-03-31 21:50:37 +0800

    Remove unit tests for `Reserve` as we don't need it any more

--- DanmakuEngine.Tests/DearImgui/TestImguiExtensions.cs
+++ DanmakuEngine.Tests/DearImgui/TestImguiExtensions.cs
@@ -25,48 +25,4 @@ public class TestImguiExtensions
         Assert.That(rhs.Capacity, Is.EqualTo(2));
         Assert.That(rhs.Data, Is.EqualTo((nint)3));
     }
-
-    [Test]
-    public void TestImVectorReserveLess()
-    {
-        var ram = Marshal.AllocHGlobal(8);
-        ImVector lhs = new(4, 8, ram);
-
-        unsafe
-        {
-            lhs.Reserve<int>(4);
-        }
-
-        try
-        {
-            Assert.That(lhs.Data, Is.EqualTo((nint)ram));
-            Assert.That(lhs.Capacity, Is.EqualTo(8));
-        }
-        finally
-        {
-            Marshal.FreeHGlobal(ram);
-        }
-    }
-
-    [Test]
-    public void TestImVectorReserveMore()
-    {
-        var ram = Marshal.AllocHGlobal(4);
-        ImVector lhs = new(1, 4, ram);
-
-        unsafe
-        {
-            lhs.Reserve<int>(8);
-        }
-
-        try
-        {
-            Assert.That(lhs.Data, Is.Not.EqualTo((nint)ram));
-            Assert.That(lhs.Capacity, Is.EqualTo(8));
-        }
-        finally
-        {
-            Marshal.FreeHGlobal(lhs.Data);
-        }
-    }
 }
