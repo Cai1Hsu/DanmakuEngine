6843195fea8ddd1a6dc6feb050047249306c4d77
Caiyi Hsu <cai1hsu@outlook.com>
2024-03-31 21:01:18 +0800

    Avoid using pin in `ImVector.Swap`

--- DanmakuEngine/DearImgui/ImguiExtensions.cs
+++ DanmakuEngine/DearImgui/ImguiExtensions.cs
@@ -11,24 +11,20 @@ public static unsafe class ImguiExtensions
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static void Swap(this ref ImVector lhs, ref ImVector rhs)
     {
-        fixed (void* p_lhs = &lhs)
-        fixed (void* p_rhs = &rhs)
-        {
-            ImguiVector* _lhs = (ImguiVector*)p_lhs;
-            ImguiVector* _rhs = (ImguiVector*)p_rhs;
+        var _lhs = (ImguiVector*)Unsafe.AsPointer(ref lhs);
+        var _rhs = (ImguiVector*)Unsafe.AsPointer(ref rhs);
 
-            int rhs_size = _rhs->Size;
-            _rhs->Size = _lhs->Size;
-            _lhs->Size = rhs_size;
+        int rhs_size = _rhs->Size;
+        _rhs->Size = _lhs->Size;
+        _lhs->Size = rhs_size;
 
-            int rhs_capacity = _rhs->Capacity;
-            _rhs->Capacity = _lhs->Capacity;
-            _lhs->Capacity = rhs_capacity;
+        int rhs_capacity = _rhs->Capacity;
+        _rhs->Capacity = _lhs->Capacity;
+        _lhs->Capacity = rhs_capacity;
 
-            nint rhs_data = _rhs->Data;
-            _rhs->Data = _lhs->Data;
-            _lhs->Data = rhs_data;
-        }
+        nint rhs_data = _rhs->Data;
+        _rhs->Data = _lhs->Data;
+        _lhs->Data = rhs_data;
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
