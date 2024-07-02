dced0b133b8b18ee1b53716e6fa85a33e31fd042
Caiyi Shyu <cai1hsu@outlook.com>
2024-07-02 19:00:08 +0800

    feat: Add NativeMemory class for allocating native memory, close #83
    The code changes introduce a new `NativeMemory` class in the `DanmakuEngine.Allocations` namespace. This class is a wrapper for `System.Runtime.InteropServices.NativeMemory` with fallback for older machines that does not support Universal C Runtime.

--- DanmakuEngine/Engine/Platform/Linux/LinuxGameHost.cs
+++ DanmakuEngine/Engine/Platform/Linux/LinuxGameHost.cs
@@ -1,9 +1,9 @@
-using System.Runtime.InteropServices;
 using System.Runtime.Versioning;
 using DanmakuEngine.Engine.Platform.Environments;
 using DanmakuEngine.Engine.Platform.Environments.Xdg;
 using DanmakuEngine.Engine.Platform.Linux.X11;
 using DanmakuEngine.Engine.SDLNative;
+using DanmakuEngine.Allocations;
 using DanmakuEngine.Extensions;
 using DanmakuEngine.Logging;
 using OpenTK.Windowing.GraphicsLibraryFramework;
@@ -112,13 +112,13 @@ public unsafe partial class LinuxGameHost : DesktopGameHost
                         var mode = new SDL_DisplayMode
                         {
                             Format = format,
-                            Driverdata = (SDL_DisplayModeData*)Marshal.AllocHGlobal(sizeof(SDL_DisplayModeData)),
+                            Driverdata = (SDL_DisplayModeData*)NativeMemory.Alloc((nuint)sizeof(SDL_DisplayModeData)),
                         };
 
                         if (!setXRandRModeInfo(display, screen, output_info->crtc,
                             output_info->modes[j], ref mode))
                         {
-                            Marshal.FreeHGlobal((IntPtr)mode.Driverdata);
+                            NativeMemory.Free(mode.Driverdata);
                             continue;
                         }
 
