0b01d191dff095d72591950f311b827ee080012f
Caiyi Shyu <cai1hsu@outlook.com>
2024-07-21 21:44:22 +0800

    refactor: Optimize color conversion methods in Color4Extensions

--- DanmakuEngine/Extensions/Color4Extensions.cs
+++ DanmakuEngine/Extensions/Color4Extensions.cs
@@ -3,6 +3,7 @@
 using System.Numerics;
 using System.Globalization;
 using DanmakuEngine.Graphics.Colors;
+using System.Runtime.CompilerServices;
 
 namespace DanmakuEngine.Extensions;
 
@@ -14,6 +15,7 @@ public static class ColorExtensions
     // MathF.Pow performs way faster than Math.Pow, however on Windows it lacks a fast path for x == 1.
     // Given passing color == 1 (White or Transparent) is very common, a fast path for 1 is added.
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
     public static double ToLinear(double color)
     {
         if (color == 1)
@@ -22,6 +24,7 @@ public static class ColorExtensions
         return color <= 0.04045 ? color / 12.92 : Math.Pow((color + 0.055) / 1.055, GAMMA);
     }
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
     public static float ToLinear(float color)
     {
         if (color == 1)
@@ -30,6 +33,7 @@ public static class ColorExtensions
         return color <= 0.04045f ? color / 12.92f : MathF.Pow((color + 0.055f) / 1.055f, (float)GAMMA);
     }
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
     public static double ToSRGB(double color)
     {
         if (color == 1)
@@ -38,6 +42,7 @@ public static class ColorExtensions
         return color < 0.0031308 ? 12.92 * color : 1.055 * Math.Pow(color, 1.0 / GAMMA) - 0.055;
     }
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
     public static float ToSRGB(float color)
     {
         if (color == 1)
@@ -48,15 +53,15 @@ public static class ColorExtensions
 
     public static Vector4 ToLinearVector4(this SRGBColor Color) =>
         new Vector4(
-            ToLinear(Color.R),
-            ToLinear(Color.G),
-            ToLinear(Color.B),
+            ToLinear(Color.R / 255f),
+            ToLinear(Color.G / 255f),
+            ToLinear(Color.B / 255f),
             Color.A);
 
     public static SRGBColor FromLinearVector4(Vector4 Color) =>
         new SRGBColor(
-            ToSRGB(Color.X),
-            ToSRGB(Color.Y),
-            ToSRGB(Color.Z),
+            ToSRGB(Color.X) * 255f,
+            ToSRGB(Color.Y) * 255f,
+            ToSRGB(Color.Z) * 255f,
             Color.W);
 }
