08dfe89bcaa9ef5abc9fb454061c6edbdd73f09d
Caiyi Shyu <cai1hsu@outlook.com>
2024-07-21 19:44:55 +0800

    Fallback to use hsl interpolation

--- DanmakuEngine/Transformation/ColorLerpHandle.cs
+++ DanmakuEngine/Transformation/ColorLerpHandle.cs
@@ -6,77 +6,61 @@ using DanmakuEngine.Utils;
 namespace DanmakuEngine.Transformation;
 
 /// <summary>
-/// A handle for lerping between two colors. Using HSL color space and Quaternion spherical linear interpolation.
+/// A handle for lerping between two colors. Using HSL color space.
 /// </summary>
 public class ColorLerpHandle : ILerpHandle<RGBAColor>
 {
-    private Quaternion _start;
-    private Quaternion _end;
+    private HkSLColor _start;
+    private HkSLColor _end;
 
     private float _startAlpha;
     private float _endAlpha;
 
     public ColorLerpHandle(RGBAColor start, RGBAColor end)
+        : this(HkSLColor.FromRGBA(start), HkSLColor.FromRGBA(end), start.A, end.A)
     {
-        _start = hslToQuaternion(HkSLColor.FromRGBA(start));
-        _end = hslToQuaternion(HkSLColor.FromRGBA(end));
-
-        _startAlpha = start.A;
-        _endAlpha = end.A;
     }
 
+    public ColorLerpHandle(HkSLColor start, HkSLColor end, float startAlpha = 1.0f, float endAlpha = 1.0f)
+    {
+        _start = start;
+        _end = end;
+
+        _startAlpha = startAlpha;
+        _endAlpha = endAlpha;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveOptimization)]
     public RGBAColor Lerp(float t)
     {
-        var lerped = Quaternion.Slerp(_start, _end, t);
+        HkSLColor hsl;
 
-        var hsl = quaternionToHSL(lerped);
+        float ah = _start.Hk, bh = _end.Hk;
+        float delta = bh - ah;
+
+        float ht;
+        if (ah > bh)
+        {
+            (ah, bh) = (bh, ah);
+
+            delta = -delta;
+            ht = 1 - t;
+        }
+        else
+        {
+            ht = t;
+        }
+
+        if (delta > 0.5f)
+            hsl.Hk = MathUtils.Lerp(ah + 1, bh, ht) % 1;
+        else
+            hsl.Hk = ah + delta * ht;
+
+        hsl.S = MathUtils.Lerp(_start.S, _end.S, t);
+        hsl.L = MathUtils.Lerp(_start.L, _end.L, t);
 
         float a = MathUtils.Lerp(_startAlpha, _endAlpha, t);
 
         return hsl.ToRGBAColor(a / 255f);
-    }
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
-    private static Quaternion hslToQuaternion(HkSLColor hksl)
-    {
-        float radius = hksl.S;
-
-        float halfHueRad = hksl.Hk * MathF.Tau / 2;
-        float halfLightnessRad = hksl.L * MathF.PI / 2;
-
-        float CosHalfHue = MathF.Cos(halfHueRad);
-        float SinHalfHue = MathF.Sin(halfHueRad);
-
-        float CosHalfLightness = MathF.Cos(halfLightnessRad);
-        float SinHalfLightness = MathF.Sin(halfLightnessRad);
-
-        return new Quaternion
-        {
-            W = CosHalfHue * CosHalfLightness,
-            X = SinHalfHue * CosHalfLightness,
-            Y = CosHalfHue * SinHalfLightness,
-            Z = SinHalfHue * SinHalfLightness * radius,
-        };
-    }
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
-    private static HkSLColor quaternionToHSL(Quaternion quaternion)
-    {
-        float radius = quaternion.Length();
-
-        float lightnessRad = MathF.Atan2(quaternion.Y, quaternion.W) * 2;
-        float hueRad = MathF.Atan2(quaternion.X, quaternion.W) * 2;
-
-        var color = new HkSLColor
-        {
-            Hk = hueRad / MathF.Tau,
-            S = radius,
-            L = lightnessRad / MathF.PI,
-        };
-
-        if (color.Hk < 0)
-            color.Hk += 1;
-
-        return color;
     }
 }
