66b5777d3cb4b9c2f1a36f1ceef29893d21a92f9
Caiyi Shyu <cai1hsu@outlook.com>
2024-07-22 15:48:50 +0800

    refactor: Update LabColor struct to use float instead of double for calculations

--- DanmakuEngine/Graphics/Colors/LabColor.cs
+++ DanmakuEngine/Graphics/Colors/LabColor.cs
@@ -46,37 +46,37 @@ public struct LabColor
     public LchColor ToLch()
         => LchColor.FromLab(this);
 
-    private const double xn = 0.96422;
-    private const double yn = 1.0;
-    private const double zn = 0.82521;
-    private const double t0 = 4.0 / 29.0;
-    private const double t1 = 6.0 / 29.0;
-    private const double t2 = 3.0 * t1 * t1;
-    private const double t3 = t1 * t1 * t1;
+    private const float xn = 0.96422f;
+    private const float yn = 1.0f;
+    private const float zn = 0.82521f;
+    private const float t0 = 4.0f / 29.0f;
+    private const float t1 = 6.0f / 29.0f;
+    private const float t2 = 3.0f * t1 * t1;
+    private const float t3 = t1 * t1 * t1;
 
     [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
-    private static double xyz2Lab(double t)
-        => t > t3 ? Math.Pow(t, 1.0 / 3.0) : t / t2 + t0;
+    private static float xyz2Lab(float t)
+        => t > t3 ? MathF.Pow(t, 1.0f / 3.0f) : t / t2 + t0;
 
     [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
-    private static double lab2Xyz(double t)
+    private static float lab2Xyz(float t)
         => t > t1 ? t * t * t : t2 * (t - t0);
 
     [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
     private static SRGBColor toSRGBInternal(LabColor lab)
     {
-        float y = (lab.L + 16) / 116,
-            x = float.IsNaN(lab.A) ? y : y + lab.A / 500,
-            z = float.IsNaN(lab.B) ? y : y - lab.B / 200;
+        float y = (lab.L + 16f) / 116f,
+            x = float.IsNaN(lab.A) ? y : y + lab.A / 500f,
+            z = float.IsNaN(lab.B) ? y : y - lab.B / 200f;
 
-        double newX = lab2Xyz(x) * xn;
-        double newY = lab2Xyz(y) * yn;
-        double newZ = lab2Xyz(z) * zn;
+        float newX = lab2Xyz(x) * xn;
+        float newY = lab2Xyz(y) * yn;
+        float newZ = lab2Xyz(z) * zn;
 
         return new SRGBColor(
-            (float)(ColorExtensions.ToSRGB(3.1338561 * newX - 1.6168667 * newY - 0.4906146 * newZ) * 255f),
-            (float)(ColorExtensions.ToSRGB(-0.9787684 * newX + 1.9161415 * newY + 0.0334540 * newZ) * 255f),
-            (float)(ColorExtensions.ToSRGB(0.0719453 * newX - 0.2289914 * newY + 1.4052427 * newZ) * 255f),
+            (float)(ColorExtensions.ToSRGB(+3.1338561f * newX - 1.6168667f * newY - 0.4906146f * newZ) * 255f),
+            (float)(ColorExtensions.ToSRGB(-0.9787684f * newX + 1.9161415f * newY + 0.0334540f * newZ) * 255f),
+            (float)(ColorExtensions.ToSRGB(+0.0719453f * newX - 0.2289914f * newY + 1.4052427f * newZ) * 255f),
             lab.Opacity * 255f
         );
     }
@@ -89,7 +89,7 @@ public struct LabColor
     {
         var linear = sRGBColor.ToLinear();
 
-        double x, z, y = xyz2Lab((0.2225045 * linear.X + 0.7168786 * linear.Y + 0.0606169 * linear.Z) / yn);
+        float x, z, y = xyz2Lab((0.2225045f * linear.X + 0.7168786f * linear.Y + 0.0606169f * linear.Z) / yn);
 
         if (linear.X == linear.Y && linear.Y == linear.Z)
         {
@@ -97,14 +97,14 @@ public struct LabColor
         }
         else
         {
-            x = xyz2Lab((0.4360747 * linear.X + 0.3850649 * linear.Y + 0.1430804 * linear.Z) / xn);
-            z = xyz2Lab((0.0139322 * linear.X + 0.0971045 * linear.Y + 0.7141733 * linear.Z) / zn);
+            x = xyz2Lab((0.4360747f * linear.X + 0.3850649f * linear.Y + 0.1430804f * linear.Z) / xn);
+            z = xyz2Lab((0.0139322f * linear.X + 0.0971045f * linear.Y + 0.7141733f * linear.Z) / zn);
         }
 
         return new LabColor(
-            (float)(116 * y - 16),
-            (float)(500 * (x - y)),
-            (float)(200 * (y - z)),
+            (float)(116f * y - 16f),
+            (float)(500f * (x - y)),
+            (float)(200f * (y - z)),
             sRGBColor.A / 255f
         );
     }
